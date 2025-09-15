8df32ad8c239b2d1cac8e2c7a9bdf9f4f216483c
dMcDonough <dmcdonough1@mtech.edu>
2025-09-15 10:33:07 -0600

    Moved ifndef, define, and endif from math_ops.c to math_ops.h

--- include/math_ops.h
+++ include/math_ops.h
@@ -1,13 +1,5 @@
-#include "math_ops.h"
-
-#ifndef UNITY_PROGMEM
-#define UNITY_PROGMEM
-#endif
-
-/* If omitted from header, declare overrideable prototypes here so they're ready for use */
-#ifdef UNITY_OMIT_OUTPUT_CHAR_HEADER_DECLARATION
-void UNITY_OUTPUT_CHAR(int);
-#endif
+#ifndef MATH_OPS
+#define MATH_OPS
 
 float multiply(float, float);
 float add(float, float);
@@ -16,3 +8,5 @@ float cube(float);
 float pow(float, int);
 float sub(float, float);
 float divide(float, float);
+
+#endif
