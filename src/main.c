c630afa4a1c1aa512c91ac9b4ac1cd8cbfdca0a5
dMcDonough <dmcdonough1@mtech.edu>
2025-09-15 10:14:53 -0600

    Modified main.c so that the options lined up to the functions.

--- src/main.c
+++ src/main.c
@@ -40,10 +40,10 @@ int main(int argc, char *argv[])
             printf("\nOptions:\n");
             printf("  --author             Print author name\n");
             printf("  --authorship         Print academic honesty statement\n");
-            printf("  --addition a b       Add two numbers\n");
-            printf("  --subtraction a b    Subtract two numbers\n");
-            printf("  --multiplication a b Multiply two numbers\n");
-            printf("  --division a b       Divide two numbers\n");
+            printf("  --add a b       Add two numbers\n");
+            printf("  --sub a b    Subtract two numbers\n");
+            printf("  --multiply a b Multiply two numbers\n");
+            printf("  --divide a b       Divide two numbers\n");
             printf("  --power a b          Raise a to the power of b\n");
             printf("  --square a           Square of a number\n");
             printf("  --cube a             Cube of a number\n");
